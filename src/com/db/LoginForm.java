126b636085902f31de35a305bd545890b428fd8e
PogChamp <stalkercxz@yandex.ru>
2017-02-15 10:22:51 +0300

    Adding program to rep

---
+++ src/com/db/LoginForm.java
@@ -0,0 +1,10 @@
+package com.db;
+
+import javax.swing.*;
+
+/**
+ * Created by Jabarda on 07.12.2016.
+ */
+public class LoginForm {
+    private JPanel panel1;
+}
---
+++ src/com/db/Main.java
@@ -0,0 +1,40 @@
+package com.db;
+import java.sql.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5432/testdb",
+                            "postgres", "0192837465");
+            System.out.println("Opened database successfully");
+
+            stmt = c.createStatement();
+            String sql = "CREATE TABLE COMPANY1 " +
+                    "(ID INT PRIMARY KEY     NOT NULL," +
+                    " NAME           TEXT    NOT NULL, " +
+                    " AGE            INT     NOT NULL, " +
+                    " ADDRESS        CHAR(50), " +
+                    " SALARY         REAL)";
+            stmt.executeUpdate(sql);
+            sql = "CREATE TABLE JABARDA " +
+                    "(ID INT PRIMARY KEY     NOT NULL," +
+                    " NAME           TEXT    NOT NULL, " +
+                    " AGE            INT     NOT NULL, " +
+                    " ADDRESS        CHAR(50), " +
+                    " SALARY         REAL)";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.close();
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Table created successfully");
+    }
+}
+
